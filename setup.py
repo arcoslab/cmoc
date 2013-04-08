3122d28325e6630847c2d3bec9101b60fe1948d4
memeruiz <fruiz@eie.ucr.ac.cr>
2013-04-08 02:17:36 -0600

    planar sliding simple. xfinger_feeder, LS added.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='cmoc',
       author_email='memeruiz@gmail.com',
       url='http://www.arcoslab.org/',
       package_dir={'cmoc': ''},
-      packages=['cmoc', 'cmoc.robot'],
-      scripts=['robot/joint_sim', 'robot/torque_sim', 'robot/sahand_yarp_sim']
+      packages=['cmoc', 'cmoc.robot', 'cmoc.objects', 'cmoc.objects.sliding'],
+      scripts=['robot/joint_sim', 'robot/torque_sim', 'robot/sahand_yarp_sim', 'objects/sliding/planar_sliding_simple', 'objects/sliding/tools/xfinger_feeder']
      )
 
